4325344247ece6148becdea109ad5698c028689e
strini <naicker.strini7@gmail.com>
2024-06-03 21:05:58 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Playstation Legends</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="fixed-top navbar navbar-expand-lg dark-dark bg-dark">
+  <a class="navbar-brand text-light" href="#">Playstation Legends</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link text-light" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="#about">About Me</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-light" href="/index.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Legends
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#nathan">Nathan Drake</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#kratos">Kratos</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#jin">Jin Sakai</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#peter">Peter Parker & Miles Morales</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#ellie">Ellie</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#ratchet">Ratchet & Clank</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#aloy">Aloy</a>
+        </div>
+        <li class="nav-item">
+        <a class="nav-link text-light" href="#contact">Contact Me</a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block" src="images/kratos2.jpg" alt="Kratos">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block" src="images/nate9.jpg" alt="Nate">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block" src="images/aloy7.jpg" alt="Aloy">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block" src="images/rac4.jpg" alt="Ratchet">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block" src="images/got1.jpg" alt="Ghost">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block" src="images/sp3.jpg" alt="Peter">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block" src="images/tlou3.jpg" alt="Ellie">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+
+<a id="nathan">
+<section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Nathan Drake</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/nate10.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/nate7.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/nate8.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/nate12.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/nate13.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/nate14.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="kratos">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Kratos</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/kratos1.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/kratos3.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/kratos4.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/kratos5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/kratos6.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/kratos8.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="jin">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Jin Sakai</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/got2.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/got3.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/got4.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/got5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/got6.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/got7.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="peter">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Peter Parker & Miles Morales</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/sp1.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/sp2.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/sp4.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/sp5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/sp6.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/sp7.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="ellie">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Ellie</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/tlou1.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/tlou2.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/tlou5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/tlou4.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/tlou6.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/tlou7.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="ratchet">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Ratchet & Clank</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/rac1.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/rac2.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/rac3.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/rac5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/rac6.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/rac7.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="aloy">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">Aloy</h2>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/aloy1.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/aloy2.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/aloy3.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/aloy4.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/aloy5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/aloy6.jpg" class="img-fluid pb-3">
+        </div>
+      </div>
+    </div>
+  </section>
+  </a>
+
+  <a id="about">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="context-text">About Me</h2>
+    </div>
+    <div class="container-fluid">
+      <h3 class="text-center">Hi there, I'm Strini.</h3> <br>
+      <p class="text-center"><b>I am an aspiring front end developer always looking for the next project to create. <br> This particular project showcases the greatest Playstation characters of all time.</b></p>
+    </div>
+</section>
+  </a>
+
+  <a id="contact">
+  <section class="my-4">
+    <div class="py-4">
+      <h2 class="contact-text">Contact Me</h2>
+    </div>
+
+    <div class="w-50 m-auto">
+      <form action="about.php" method="post">
+        <div class="form-group">
+          <label>Name:</label>
+          <input type="text" name="name" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Email:</label>
+          <input type="text" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Number</label>
+          <input type="text" name="number" class="form-control">
+        </div>
+        <button type="submit" class="btn">Submit</button>
+      </form>
+    </div>
+  </section>
+  </a>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
